4a887203a11647b48791496279f615aef845ad29
KaueSiqueira <kaue54150@gmail.com>
2026-05-29 13:32:17 -0300

    Aula 1 sintaxe java

---
+++ PrimeiroTeste/src/main/java/pacoteteste/ClasseTeste.java
@@ -0,0 +1,7 @@
+package pacoteteste;
+
+public class ClasseTeste {
+    public static void main(String[]args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ PrimeiroTeste/src/main/java/pacoteteste/ClasseTeste2.java
@@ -0,0 +1,13 @@
+package pacoteteste;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ClasseTeste2 {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite a frase:");
+        var frase1 = leitura.nextLine();
+        System.out.println(frase1);
+    }
+}
